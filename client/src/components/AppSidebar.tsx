a9aed3b1690e42f90956ca8455a373bbab0fa5c4
rzkylimitx <rzkylimitx@gmail.com>
2025-02-11 06:40:41 +0700

    Update AppSidebar.tsx

--- client/src/components/AppSidebar.tsx
+++ client/src/components/AppSidebar.tsx
@@ -76,7 +76,7 @@ const AppSidebar = () => {
                     height={20}
                     className="app-sidebar__logo"
                   />
-                  <p className="app-sidebar__title">CIHUY</p>
+                  <p className="app-sidebar__title">KEIKEN</p>
                 </div>
                 <PanelLeft className="app-sidebar__collapse-icon" />
               </div>
